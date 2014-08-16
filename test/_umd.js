0ed43688854bb1853e7e8f6e707b4c2dc9a365ee
Jarid Margolin <jaridmargolin@gmail.com>
2014-08-16 14:21:29 -0700

    Finish integration with easy-build.

---
+++ test/_umd.js
@@ -0,0 +1,30 @@
+/*
+ * test/_umd.js
+ * 
+ * Copyright (c) 2014
+ */
+
+define([
+  'proclaim',
+  'assist/assist'
+], function (assert, _) {
+
+
+/* -----------------------------------------------------------------------------
+ * test
+ * ---------------------------------------------------------------------------*/
+
+describe('umd - assist.js', function () {
+
+  it('Should expose methods.', function () {
+    assert.ok(_.decorate);
+    assert.ok(_.evaluate);
+    assert.ok(_.execute);
+    assert.ok(_.parametize);
+    assert.ok(_.snip);
+  });
+
+});
+
+
+});
