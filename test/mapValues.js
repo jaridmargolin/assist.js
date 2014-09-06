b2402318f7315999a90cd34845302eeda1caf8a1
Jarid Margolin <jaridmargolin@gmail.com>
2014-09-06 14:24:47 -0700

    Add `mapValues` method. Close #1.

---
+++ test/mapValues.js
@@ -0,0 +1,45 @@
+/*!
+ * test/mapValues.js
+ * 
+ * Copyright (c) 2014
+ */
+
+define([
+  'underscore',
+  'proclaim',
+  'sinon',
+  'mapValues'
+], function (_, assert, sinon, mapValues) {
+
+
+/* -----------------------------------------------------------------------------
+ * test
+ * ---------------------------------------------------------------------------*/
+
+describe('mapValues.js', function () {
+
+  beforeEach(function () {
+    this.toMap = { 'key': 'value' };
+  });
+
+  it('Should returns a new object with mapped values.', function () {
+    var mapped = mapValues(this.toMap, function (value, key) {
+      return 'mapped-' + value;
+    });
+
+    assert.deepEqual(mapped, { 'key': 'mapped-value' });
+  });
+
+  it('Should not alter original object.', function () {
+    var mapped = mapValues(this.toMap, function (value, key) {
+      return 'mapped-' + value;
+    });
+
+    assert.deepEqual(this.toMap, { 'key': 'value' });
+  });
+
+
+});
+
+
+});
