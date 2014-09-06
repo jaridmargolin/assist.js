a41de2ee9a0efc095ad9f326fb4bfc6de872d4bb
Jarid Margolin <jaridmargolin@gmail.com>
2014-09-06 14:21:27 -0700

    Add assist.js test to check for exposed methods.

---
+++ test/assist.js
@@ -0,0 +1,32 @@
+/*
+ * test/assist.js
+ * 
+ * Copyright (c) 2014
+ */
+
+define([
+  'proclaim',
+  'assist'
+], function (assert, _) {
+
+
+/* -----------------------------------------------------------------------------
+ * test
+ * ---------------------------------------------------------------------------*/
+
+describe('assist.js', function () {
+
+  it('Should expose methods.', function () {
+    assert.ok(_.decorate);
+    assert.ok(_.deepMerge);
+    assert.ok(_.evaluate);
+    assert.ok(_.execute);
+    assert.ok(_.jsonClone);
+    assert.ok(_.parametize);
+    assert.ok(_.snip);
+  });
+
+});
+
+
+});
