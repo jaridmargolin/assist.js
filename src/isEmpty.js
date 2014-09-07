2f3592639461f8bf0f2f79d8d8cb1f7ffb207153
Jarid Margolin <jaridmargolin@gmail.com>
2014-09-07 14:29:34 -0700

    Add `isEmpty` method. Close #6.

---
+++ src/isEmpty.js
@@ -0,0 +1,43 @@
+/*!
+ * isEmpty.js
+ * 
+ * Copyright (c) 2014
+ */
+
+define([
+  'isArray'
+], function (isArray) {
+
+
+/* -----------------------------------------------------------------------------
+ * isEmpty
+ * ---------------------------------------------------------------------------*/
+
+/**
+ * Determine if an object or array is empty.
+ *
+ * @example
+ * var empty = isEmpty({});
+ * // true
+ *
+ * @public
+ *
+ * @param {object} obj - obj to run isEmpty test on
+ * @reutns {boolean} - true if object is empty. false if not.
+ */
+return function (obj) {
+  // Array
+  if (isArray(obj)) {
+    return (obj.length === 0);
+  }
+
+  // Object
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop)) { return false; }
+  }
+
+  return true;
+};
+
+
+});
